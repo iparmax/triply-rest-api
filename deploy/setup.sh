48e85a24d364bfac426f73e1a76d9cd04bcc0d97
Ilias Parmaksizoglou <iparmaksizoglou@gmail.com>
2020-05-14 21:28:43 +0300

    deployment

---
+++ deploy/setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+# TODO: Set to URL of git repo.
+PROJECT_GIT_URL='https://github.com/iparmax/triply-rest-api.git'
+
+PROJECT_BASE_PATH='/usr/local/apps/triply-rest-api'
+
+echo "Installing dependencies..."
+apt-get update
+apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+
+# Create project directory
+mkdir -p $PROJECT_BASE_PATH
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
+
+# Create virtual environment
+mkdir -p $PROJECT_BASE_PATH/env
+python3 -m venv $PROJECT_BASE_PATH/env
+
+# Install python packages
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
+
+# Run migrations and collectstatic
+cd $PROJECT_BASE_PATH
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+
+# Configure supervisor
+cp $PROJECT_BASE_PATH/deploy/supervisor_trips_api.conf /etc/supervisor/conf.d/trips_api.conf
+supervisorctl reread
+supervisorctl update
+supervisorctl restart trips_api
+
+# Configure nginx
+cp $PROJECT_BASE_PATH/deploy/nginx_trips_api.conf /etc/nginx/sites-available/trips_api.conf
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/trips_api.conf /etc/nginx/sites-enabled/trips_api.conf
+systemctl restart nginx.service
+
+echo "DONE! :)"
--- triply_project/settings.py
+++ triply_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g!3(3-gg3n%^h+bxz7n9m7em&*u0$!6(y&4==&(z0v&tp-ql_)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "trips_api.Trip"
+
+STATIC_ROOT ='static/'
