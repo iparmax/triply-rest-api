2910aa34b59fc4b8f880d338bc863520fb768584
Ilias Parmaksizoglou <iparmaksizoglou@gmail.com>
2020-05-15 19:44:57 +0300

    reconfigured local run

--- fuq.py
+++ fuq.py
@@ -1,17 +1,18 @@
 import requests
 import json
 
-url = 'http://localhost:8000/api/trips/'
-# myobj = {"email": "iparmaksizoglou@gmail.com","user_id" :"6969696969","origin": "(69.696969,69.6969696)"\
-#     ,"destination": "(69.696969,69.6969696)","dep_time":"12:00"}
-
-# x = requests.post(url, data = myobj)
+url = 'http://ec2-35-180-28-180.eu-west-3.compute.amazonaws.com/api/trips/'
 
 
+myobj = {"email": "iparmaksizoglou@gmail.com","user_id" :"6969696969","origin": "(69.696969,69.6969696)"\
+     ,"destination": "(69.696969,69.6969696)","dep_time":"12:00"}
 
-
+x = requests.post(url, data = myobj)
 
 
 
-x = requests.get(url).json()
-print(x)
+
+
+
+# x = requests.get(url).json()
+# print(x)
