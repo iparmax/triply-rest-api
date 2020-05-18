40b7ac44cef0ea9ca4f15dd901dc0420bdbef062
Ilias Parmaksizoglou <iparmaksizoglou@gmail.com>
2020-05-18 20:26:44 +0300

    fixed db

--- triply_project/settings.py
+++ triply_project/settings.py
@@ -87,7 +87,8 @@ WSGI_APPLICATION = 'triply_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
